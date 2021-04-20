b4085f31f7885e262e19f05be83e747d6aaf3381
Trystan <trystandames08@gmail.com>
2021-04-20 11:31:30 +0200

    second commit

--- Analysis/analysis_queries.sql
+++ Analysis/analysis_queries.sql
@@ -51,3 +51,205 @@ WHERE last_name LIKE 'S%'
 
 COPY teachers TO 'C:\Users\User\Desktop\SQL\teacherstest.txt'
 WITH (FORMAT CSV, HEADER, DELIMITER '|');
+
+Chap3
+Page 65
+CREATE TABLE number_data_types (
+numeric_column numeric(20,5),
+real_column real,
+double_column double precision
+);
+
+INSERT INTO number_data_types
+VALUES
+(.7, .7, .7),
+(2.13579, 2.13579, 2.13579),
+(2.1357987654, 2.1357987654, 2.1357987654);
+
+SELECT * FROM number_data_types;
+
+SELECT
+numeric_column * 10000000 AS "Fixed",
+real_column * 10000000 AS "Float"
+FROM number_data_types
+WHERE numeric_column = .7;
+
+CREATE TABLE date_time_types (
+timestamp_column timestamp with time zone,
+interval_column interval
+);
+
+INSERT INTO date_time_types
+VALUES
+('2018-12-31 01:00 EST','2 days'),
+('2018-12-31 01:00 -8','1 month'),
+('2018-12-31 01:00 Australia/Melbourne','1 century'),
+(now(),'1 week');
+
+SELECT * FROM date_time_types;
+
+SELECT
+timestamp_column,
+interval_column,
+timestamp_column - interval_column AS new_date
+FROM date_time_types;
+
+Chap4
+Page 79
+
+CREATE TABLE us_counties_2010 (
+geo_name varchar(90),
+state_us_abbreviation varchar(2),
+summary_level varchar(3),
+region smallint,
+division smallint,
+state_fips varchar(2),
+county_fips varchar(3),
+area_land bigint,
+area_water bigint,
+population_count_100_percent integer,
+housing_unit_count_100_percent integer,
+internal_point_lat numeric(10,7),
+internal_point_lon numeric(10,7),
+	
+ p0010001 integer,   -- Total population
+    p0010002 integer,   -- Population of one race:
+    p0010003 integer,       -- White Alone
+    p0010004 integer,       -- Black or African American alone
+    p0010005 integer,       -- American Indian and Alaska Native alone
+    p0010006 integer,       -- Asian alone
+    p0010007 integer,       -- Native Hawaiian and Other Pacific Islander alone
+    p0010008 integer,       -- Some Other Race alone
+    p0010009 integer,   -- Population of two or more races
+    p0010010 integer,   -- Population of two races:
+    p0010011 integer,       -- White; Black or African American
+    p0010012 integer,       -- White; American Indian and Alaska Native
+    p0010013 integer,       -- White; Asian
+    p0010014 integer,       -- White; Native Hawaiian and Other Pacific Islander
+    p0010015 integer,       -- White; Some Other Race
+    p0010016 integer,       -- Black or African American; American Indian and Alaska Native
+    p0010017 integer,       -- Black or African American; Asian
+    p0010018 integer,       -- Black or African American; Native Hawaiian and Other Pacific Islander
+    p0010019 integer,       -- Black or African American; Some Other Race
+    p0010020 integer,       -- American Indian and Alaska Native; Asian
+    p0010021 integer,       -- American Indian and Alaska Native; Native Hawaiian and Other Pacific Islander
+    p0010022 integer,       -- American Indian and Alaska Native; Some Other Race
+    p0010023 integer,       -- Asian; Native Hawaiian and Other Pacific Islander
+    p0010024 integer,       -- Asian; Some Other Race
+    p0010025 integer,       -- Native Hawaiian and Other Pacific Islander; Some Other Race
+    p0010026 integer,   -- Population of three races
+    p0010047 integer,   -- Population of four races
+    p0010063 integer,   -- Population of five races
+    p0010070 integer,   -- Population of six races
+
+    -- This section is referred to as P2. HISPANIC OR LATINO, AND NOT HISPANIC OR LATINO BY RACE
+    p0020001 integer,   -- Total
+    p0020002 integer,   -- Hispanic or Latino
+    p0020003 integer,   -- Not Hispanic or Latino:
+    p0020004 integer,   -- Population of one race:
+    p0020005 integer,       -- White Alone
+    p0020006 integer,       -- Black or African American alone
+    p0020007 integer,       -- American Indian and Alaska Native alone
+    p0020008 integer,       -- Asian alone
+    p0020009 integer,       -- Native Hawaiian and Other Pacific Islander alone
+    p0020010 integer,       -- Some Other Race alone
+    p0020011 integer,   -- Two or More Races
+    p0020012 integer,   -- Population of two races
+    p0020028 integer,   -- Population of three races
+    p0020049 integer,   -- Population of four races
+    p0020065 integer,   -- Population of five races
+    p0020072 integer,   -- Population of six races
+
+    -- This section is referred to as P3. RACE FOR THE POPULATION 18 YEARS AND OVER
+    p0030001 integer,   -- Total
+    p0030002 integer,   -- Population of one race:
+    p0030003 integer,       -- White alone
+    p0030004 integer,       -- Black or African American alone
+    p0030005 integer,       -- American Indian and Alaska Native alone
+    p0030006 integer,       -- Asian alone
+    p0030007 integer,       -- Native Hawaiian and Other Pacific Islander alone
+    p0030008 integer,       -- Some Other Race alone
+    p0030009 integer,   -- Two or More Races
+    p0030010 integer,   -- Population of two races
+    p0030026 integer,   -- Population of three races
+    p0030047 integer,   -- Population of four races
+    p0030063 integer,   -- Population of five races
+    p0030070 integer,   -- Population of six races
+
+    -- This section is referred to as P4. HISPANIC OR LATINO, AND NOT HISPANIC OR LATINO BY RACE
+    -- FOR THE POPULATION 18 YEARS AND OVER
+    p0040001 integer,   -- Total
+    p0040002 integer,   -- Hispanic or Latino
+    p0040003 integer,   -- Not Hispanic or Latino:
+    p0040004 integer,   -- Population of one race:
+    p0040005 integer,   -- White alone
+    p0040006 integer,   -- Black or African American alone
+    p0040007 integer,   -- American Indian and Alaska Native alone
+    p0040008 integer,   -- Asian alone
+    p0040009 integer,   -- Native Hawaiian and Other Pacific Islander alone
+    p0040010 integer,   -- Some Other Race alone
+    p0040011 integer,   -- Two or More Races
+    p0040012 integer,   -- Population of two races
+    p0040028 integer,   -- Population of three races
+    p0040049 integer,   -- Population of four races
+    p0040065 integer,   -- Population of five races
+    p0040072 integer,   -- Population of six races
+
+    -- This section is referred to as H1. OCCUPANCY STATUS
+    h0010001 integer,   -- Total housing units
+    h0010002 integer,   -- Occupied
+    h0010003 integer    -- Vacant
+);
+
+SELECT * from us_counties_2010;
+
+COPY us_counties_2010
+FROM 'C:\Users\User\Desktop\SQL\practical-sql-master\Chapter_04\us_counties_2010.csv'
+WITH (FORMAT CSV, HEADER);
+
+CREATE TABLE supervisor_salaries (
+town varchar(30),
+county varchar(30),
+supervisor varchar(30),
+start_date date,
+salary money,
+benefits money
+);
+
+COPY supervisor_salaries (town, supervisor, salary)
+FROM 'C:\Users\User\Desktop\SQL\practical-sql-master\Chapter_04\supervisor_salaries.csv'
+WITH (FORMAT CSV, HEADER);
+
+SELECT * FROM supervisor_salaries LIMIT 3;
+
+DELETE FROM supervisor_salaries;
+
+CREATE TEMPORARY TABLE supervisor_salaries_temp (LIKE supervisor_salaries);
+
+COPY supervisor_salaries_temp (town, supervisor, salary)
+FROM 'C:\Users\User\Desktop\SQL\practical-sql-master\Chapter_04\supervisor_salaries.csv'
+WITH (FORMAT CSV, HEADER);
+
+INSERT INTO supervisor_salaries (town, county, supervisor, salary)
+SELECT town, 'Some County', supervisor, salary
+FROM supervisor_salaries_temp;
+
+DROP TABLE supervisor_salaries_temp;
+
+SELECT * FROM supervisor_salaries LIMIT 3;
+
+COPY us_counties_2010
+TO 'C:\Users\User\Desktop\SQL\My Queries\Analysis\us_counties_export.txt'
+WITH (FORMAT CSV, HEADER, DELIMITER '|');
+
+COPY us_counties_2010 (geo_name, internal_point_lat, internal_point_lon)
+TO 'C:\Users\User\Desktop\SQL\My Queries\Analysis\us_counties_latlon_export.txt'
+WITH (FORMAT CSV, HEADER, DELIMITER '|');
+
+COPY (
+SELECT geo_name, state_us_abbreviation
+FROM us_counties_2010
+WHERE geo_name ILIKE '%mill%'
+)
+TO 'C:\Users\User\Desktop\SQL\My Queries\Analysis\us_counties_mill_export.txt'
+WITH (FORMAT CSV, HEADER, DELIMITER '|');
--- Classwork/TryItYourselfs.sql
+++ Classwork/TryItYourselfs.sql
@@ -77,5 +77,150 @@ INSERT INTO char_data_types
 VALUES ('abc', 'abc', 'abc'),
 ('defghi', 'defghi', 'defghi');
 
-COPY char_data_types TO 'C:\Users\User\Desktop\SQL\typetest.txt'
+COPY char_data_types TO 'C:\Users\User\Desktop\SQL\My Queries\Classwork\typetest.txt'
 WITH (FORMAT CSV, HEADER, DELIMITER '|');
+
+Page 72
+
+numeric(4,1)
+This is so you can store numbers as big as 999,9 with one digit after the decimal point
+
+varchar(50)
+This is to allow the drivers first and last name to be stored as a string.
+
+This will give us an error as we cant change a string into an integer.
+
+Page 89
+#1
+CREATE TABLE actors (
+    id integer,
+    movie text,
+    actor text
+);
+
+INSERT INTO actors (id, movie, actor)
+VALUES (50, 'Mission Impossible', 'Tom Cruise')
+
+COPY actors
+FROM 'C:\Users\User\Desktop\SQL\My Queries\Classwork\movies.txt'
+WITH (FORMAT CSV, HEADER, DELIMITER '|')
+
+#2
+CREATE TABLE us_counties_2010 (
+geo_name varchar(90),
+state_us_abbreviation varchar(2),
+summary_level varchar(3),
+region smallint,
+division smallint,
+state_fips varchar(2),
+county_fips varchar(3),
+area_land bigint,
+area_water bigint,
+population_count_100_percent integer,
+housing_unit_count_100_percent integer,
+internal_point_lat numeric(10,7),
+internal_point_lon numeric(10,7),
+	
+ p0010001 integer,   -- Total population
+    p0010002 integer,   -- Population of one race:
+    p0010003 integer,       -- White Alone
+    p0010004 integer,       -- Black or African American alone
+    p0010005 integer,       -- American Indian and Alaska Native alone
+    p0010006 integer,       -- Asian alone
+    p0010007 integer,       -- Native Hawaiian and Other Pacific Islander alone
+    p0010008 integer,       -- Some Other Race alone
+    p0010009 integer,   -- Population of two or more races
+    p0010010 integer,   -- Population of two races:
+    p0010011 integer,       -- White; Black or African American
+    p0010012 integer,       -- White; American Indian and Alaska Native
+    p0010013 integer,       -- White; Asian
+    p0010014 integer,       -- White; Native Hawaiian and Other Pacific Islander
+    p0010015 integer,       -- White; Some Other Race
+    p0010016 integer,       -- Black or African American; American Indian and Alaska Native
+    p0010017 integer,       -- Black or African American; Asian
+    p0010018 integer,       -- Black or African American; Native Hawaiian and Other Pacific Islander
+    p0010019 integer,       -- Black or African American; Some Other Race
+    p0010020 integer,       -- American Indian and Alaska Native; Asian
+    p0010021 integer,       -- American Indian and Alaska Native; Native Hawaiian and Other Pacific Islander
+    p0010022 integer,       -- American Indian and Alaska Native; Some Other Race
+    p0010023 integer,       -- Asian; Native Hawaiian and Other Pacific Islander
+    p0010024 integer,       -- Asian; Some Other Race
+    p0010025 integer,       -- Native Hawaiian and Other Pacific Islander; Some Other Race
+    p0010026 integer,   -- Population of three races
+    p0010047 integer,   -- Population of four races
+    p0010063 integer,   -- Population of five races
+    p0010070 integer,   -- Population of six races
+
+    -- This section is referred to as P2. HISPANIC OR LATINO, AND NOT HISPANIC OR LATINO BY RACE
+    p0020001 integer,   -- Total
+    p0020002 integer,   -- Hispanic or Latino
+    p0020003 integer,   -- Not Hispanic or Latino:
+    p0020004 integer,   -- Population of one race:
+    p0020005 integer,       -- White Alone
+    p0020006 integer,       -- Black or African American alone
+    p0020007 integer,       -- American Indian and Alaska Native alone
+    p0020008 integer,       -- Asian alone
+    p0020009 integer,       -- Native Hawaiian and Other Pacific Islander alone
+    p0020010 integer,       -- Some Other Race alone
+    p0020011 integer,   -- Two or More Races
+    p0020012 integer,   -- Population of two races
+    p0020028 integer,   -- Population of three races
+    p0020049 integer,   -- Population of four races
+    p0020065 integer,   -- Population of five races
+    p0020072 integer,   -- Population of six races
+
+    -- This section is referred to as P3. RACE FOR THE POPULATION 18 YEARS AND OVER
+    p0030001 integer,   -- Total
+    p0030002 integer,   -- Population of one race:
+    p0030003 integer,       -- White alone
+    p0030004 integer,       -- Black or African American alone
+    p0030005 integer,       -- American Indian and Alaska Native alone
+    p0030006 integer,       -- Asian alone
+    p0030007 integer,       -- Native Hawaiian and Other Pacific Islander alone
+    p0030008 integer,       -- Some Other Race alone
+    p0030009 integer,   -- Two or More Races
+    p0030010 integer,   -- Population of two races
+    p0030026 integer,   -- Population of three races
+    p0030047 integer,   -- Population of four races
+    p0030063 integer,   -- Population of five races
+    p0030070 integer,   -- Population of six races
+
+    -- This section is referred to as P4. HISPANIC OR LATINO, AND NOT HISPANIC OR LATINO BY RACE
+    -- FOR THE POPULATION 18 YEARS AND OVER
+    p0040001 integer,   -- Total
+    p0040002 integer,   -- Hispanic or Latino
+    p0040003 integer,   -- Not Hispanic or Latino:
+    p0040004 integer,   -- Population of one race:
+    p0040005 integer,   -- White alone
+    p0040006 integer,   -- Black or African American alone
+    p0040007 integer,   -- American Indian and Alaska Native alone
+    p0040008 integer,   -- Asian alone
+    p0040009 integer,   -- Native Hawaiian and Other Pacific Islander alone
+    p0040010 integer,   -- Some Other Race alone
+    p0040011 integer,   -- Two or More Races
+    p0040012 integer,   -- Population of two races
+    p0040028 integer,   -- Population of three races
+    p0040049 integer,   -- Population of four races
+    p0040065 integer,   -- Population of five races
+    p0040072 integer,   -- Population of six races
+
+    -- This section is referred to as H1. OCCUPANCY STATUS
+    h0010001 integer,   -- Total housing units
+    h0010002 integer,   -- Occupied
+    h0010003 integer    -- Vacant
+);
+
+COPY us_counties_2010
+FROM 'C:\Users\User\Desktop\SQL\practical-sql-master\Chapter_04\us_counties_2010.csv'
+WITH (FORMAT CSV, HEADER);
+
+COPY (
+    SELECT geo_name, state_us_abbreviation, housing_unit_count_100_percent
+    FROM us_counties_2010 
+	ORDER BY housing_unit_count_100_percent DESC LIMIT 20
+     )
+TO 'C:\Users\User\Desktop\SQL\My Queries\Classwork\us_counties_housing_export.txt'
+WITH (FORMAT CSV, HEADER);
+
+#3
+It will not due to the fact its only allowed up to 3 numbers before the decimal and 8 afterwards where we have 5 numbers before the decimal and 3 afterwards
