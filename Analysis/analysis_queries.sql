acedd462eb35d564310290e111da7f4ac734b33b
Trystan <trystandames08@gmail.com>
2021-04-22 11:22:30 +0200

    ninth commit

--- Analysis/analysis_queries.sql
+++ Analysis/analysis_queries.sql
@@ -590,3 +590,436 @@ EXPLAIN ANALYZE SELECT * FROM new_york_addresses
 WHERE street = 'ZWICKY AVENUE';
 
 CREATE INDEX street_idx ON new_york_addresses (street);
+
+Chap8
+Page 150
+CREATE TABLE pls_fy2014_pupld14a (
+    stabr varchar(2) NOT NULL,
+    fscskey varchar(6) CONSTRAINT fscskey2014_key PRIMARY KEY,
+    libid varchar(20) NOT NULL,
+    libname varchar(100) NOT NULL,
+    obereg varchar(2) NOT NULL,
+    rstatus integer NOT NULL,
+    statstru varchar(2) NOT NULL,
+    statname varchar(2) NOT NULL,
+    stataddr varchar(2) NOT NULL,
+    longitud numeric(10,7) NOT NULL,
+    latitude numeric(10,7) NOT NULL,
+    fipsst varchar(2) NOT NULL,
+    fipsco varchar(3) NOT NULL,
+    address varchar(35) NOT NULL,
+    city varchar(20) NOT NULL,
+    zip varchar(5) NOT NULL,
+    zip4 varchar(4) NOT NULL,
+    cnty varchar(20) NOT NULL,
+    phone varchar(10) NOT NULL,
+    c_relatn varchar(2) NOT NULL,
+    c_legbas varchar(2) NOT NULL,
+    c_admin varchar(2) NOT NULL,
+    geocode varchar(3) NOT NULL,
+    lsabound varchar(1) NOT NULL,
+    startdat varchar(10),
+    enddate varchar(10),
+    popu_lsa integer NOT NULL,
+    centlib integer NOT NULL,
+    branlib integer NOT NULL,
+    bkmob integer NOT NULL,
+    master numeric(8,2) NOT NULL,
+    libraria numeric(8,2) NOT NULL,
+    totstaff numeric(8,2) NOT NULL,
+    locgvt integer NOT NULL,
+    stgvt integer NOT NULL,
+    fedgvt integer NOT NULL,
+    totincm integer NOT NULL,
+    salaries integer,
+    benefit integer,
+    staffexp integer,
+    prmatexp integer NOT NULL,
+    elmatexp integer NOT NULL,
+    totexpco integer NOT NULL,
+    totopexp integer NOT NULL,
+    lcap_rev integer NOT NULL,
+    scap_rev integer NOT NULL,
+    fcap_rev integer NOT NULL,
+    cap_rev integer NOT NULL,
+    capital integer NOT NULL,
+    bkvol integer NOT NULL,
+    ebook integer NOT NULL,
+    audio_ph integer NOT NULL,
+    audio_dl float NOT NULL,
+    video_ph integer NOT NULL,
+    video_dl float NOT NULL,
+    databases integer NOT NULL,
+    subscrip integer NOT NULL,
+    hrs_open integer NOT NULL,
+    visits integer NOT NULL,
+    referenc integer NOT NULL,
+    regbor integer NOT NULL,
+    totcir integer NOT NULL,
+    kidcircl integer NOT NULL,
+    elmatcir integer NOT NULL,
+    loanto integer NOT NULL,
+    loanfm integer NOT NULL,
+    totpro integer NOT NULL,
+    totatten integer NOT NULL,
+    gpterms integer NOT NULL,
+    pitusr integer NOT NULL,
+    wifisess integer NOT NULL,
+    yr_sub integer NOT NULL
+);
+
+CREATE INDEX libname2014_idx ON pls_fy2014_pupld14a (libname);
+CREATE INDEX stabr2014_idx ON pls_fy2014_pupld14a (stabr);
+CREATE INDEX city2014_idx ON pls_fy2014_pupld14a (city);
+CREATE INDEX visits2014_idx ON pls_fy2014_pupld14a (visits);
+
+COPY pls_fy2014_pupld14a
+FROM 'C:\Users\User\Desktop\SQL\practical-sql-master\Chapter_08\pls_fy2014_pupld14a.csv'
+WITH (FORMAT CSV, HEADER);
+
+CREATE TABLE pls_fy2009_pupld09a (
+    stabr varchar(2) NOT NULL,
+    fscskey varchar(6) CONSTRAINT fscskey2009_key PRIMARY KEY,
+    libid varchar(20) NOT NULL,
+    libname varchar(100) NOT NULL,
+    address varchar(35) NOT NULL,
+    city varchar(20) NOT NULL,
+    zip varchar(5) NOT NULL,
+    zip4 varchar(4) NOT NULL,
+    cnty varchar(20) NOT NULL,
+    phone varchar(10) NOT NULL,
+    c_relatn varchar(2) NOT NULL,
+    c_legbas varchar(2) NOT NULL,
+    c_admin varchar(2) NOT NULL,
+    geocode varchar(3) NOT NULL,
+    lsabound varchar(1) NOT NULL,
+    startdat varchar(10),
+    enddate varchar(10),
+    popu_lsa integer NOT NULL,
+    centlib integer NOT NULL,
+    branlib integer NOT NULL,
+    bkmob integer NOT NULL,
+    master numeric(8,2) NOT NULL,
+    libraria numeric(8,2) NOT NULL,
+    totstaff numeric(8,2) NOT NULL,
+    locgvt integer NOT NULL,
+    stgvt integer NOT NULL,
+    fedgvt integer NOT NULL,
+    totincm integer NOT NULL,
+    salaries integer,
+    benefit integer,
+    staffexp integer,
+    prmatexp integer NOT NULL,
+    elmatexp integer NOT NULL,
+    totexpco integer NOT NULL,
+    totopexp integer NOT NULL,
+    lcap_rev integer NOT NULL,
+    scap_rev integer NOT NULL,
+    fcap_rev integer NOT NULL,
+    cap_rev integer NOT NULL,
+    capital integer NOT NULL,
+    bkvol integer NOT NULL,
+    ebook integer NOT NULL,
+    audio integer NOT NULL,
+    video integer NOT NULL,
+    databases integer NOT NULL,
+    subscrip integer NOT NULL,
+    hrs_open integer NOT NULL,
+    visits integer NOT NULL,
+    referenc integer NOT NULL,
+    regbor integer NOT NULL,
+    totcir integer NOT NULL,
+    kidcircl integer NOT NULL,
+    loanto integer NOT NULL,
+    loanfm integer NOT NULL,
+    totpro integer NOT NULL,
+    totatten integer NOT NULL,
+    gpterms integer NOT NULL,
+    pitusr integer NOT NULL,
+    yr_sub integer NOT NULL,
+    obereg varchar(2) NOT NULL,
+    rstatus integer NOT NULL,
+    statstru varchar(2) NOT NULL,
+    statname varchar(2) NOT NULL,
+    stataddr varchar(2) NOT NULL,
+    longitud numeric(10,7) NOT NULL,
+    latitude numeric(10,7) NOT NULL,
+    fipsst varchar(2) NOT NULL,
+    fipsco varchar(3) NOT NULL
+);
+
+CREATE INDEX libname2009_idx ON pls_fy2009_pupld09a (libname);
+CREATE INDEX stabr2009_idx ON pls_fy2009_pupld09a (stabr);
+CREATE INDEX city2009_idx ON pls_fy2009_pupld09a (city);
+CREATE INDEX visits2009_idx ON pls_fy2009_pupld09a (visits);
+
+COPY pls_fy2009_pupld09a
+FROM 'C:\Users\User\Desktop\SQL\practical-sql-master\Chapter_08\pls_fy2009_pupld09a.csv'
+WITH (FORMAT CSV, HEADER);
+
+SELECT count(*)
+FROM pls_fy2014_pupld14a;
+
+SELECT count(*)
+FROM pls_fy2009_pupld09a;
+
+SELECT count(salaries)
+FROM pls_fy2014_pupld14a;
+
+SELECT count(libname)
+FROM pls_fy2014_pupld14a;
+
+SELECT count(DISTINCT libname)
+FROM pls_fy2014_pupld14a;
+
+SELECT max(visits), min(visits)
+FROM pls_fy2014_pupld14a;
+
+SELECT stabr
+FROM pls_fy2014_pupld14a
+GROUP BY stabr
+ORDER BY stabr;
+
+SELECT city, stabr
+FROM pls_fy2014_pupld14a
+GROUP BY city, stabr
+ORDER BY city, stabr;
+
+SELECT stabr, count(*)
+FROM pls_fy2014_pupld14a
+GROUP BY stabr
+ORDER BY count(*) DESC;
+
+SELECT stabr, stataddr, count(*)
+FROM pls_fy2014_pupld14a
+GROUP BY stabr, stataddr
+ORDER BY stabr ASC, count(*) DESC;
+
+SELECT sum(visits) AS visits_2014
+FROM pls_fy2014_pupld14a
+WHERE visits >= 0;
+
+SELECT sum(visits) AS visits_2009
+FROM pls_fy2009_pupld09a
+WHERE visits >= 0;
+
+SELECT sum(pls09.visits) AS visits_2009,  
+sum(pls14.visits) AS visits_2014,
+sum(pls09.visits) - sum(pls14.visits) AS visits_difference
+FROM pls_fy2009_pupld09a pls09 JOIN pls_fy2014_pupld14a pls14
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+
+SELECT sum(pls14.visits) AS visits_2014,
+sum(pls09.visits) AS visits_2009
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0;
+
+SELECT pls14.stabr,
+sum(pls14.visits) AS visits_2014,
+sum(pls09.visits) AS visits_2009,
+round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY pls14.stabr
+ORDER BY pct_change DESC;
+
+SELECT pls14.stabr,
+sum(pls14.visits) AS visits_2014,
+sum(pls09.visits) AS visits_2009,
+round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY pls14.stabr
+HAVING sum(pls14.visits) > 50000000
+ORDER BY pct_change DESC;
+
+Chap 9
+Page 165
+
+CREATE TABLE meat_poultry_egg_inspect (
+est_number varchar(50) CONSTRAINT est_number_key PRIMARY KEY,
+company varchar(100),
+street varchar(100),
+city varchar(30),
+st varchar(2),
+zip varchar(5),
+phone varchar(14),
+grant_date date,
+activities text,
+dbas text
+);
+
+COPY meat_poultry_egg_inspect
+FROM 'C:\Users\User\Desktop\SQL\practical-sql-master\Chapter_09\MPI_Directory_by_Establishment_Name.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+CREATE INDEX company_idx ON meat_poultry_egg_inspect (company);
+
+SELECT count(*) FROM meat_poultry_egg_inspect;
+
+SELECT company,
+street,
+city,
+st,
+count(*) AS address_count
+FROM meat_poultry_egg_inspect
+GROUP BY company, street, city, st
+HAVING count(*) > 1
+ORDER BY company, street, city, st;
+
+SELECT st,
+count(*) AS st_count
+FROM meat_poultry_egg_inspect
+GROUP BY st
+ORDER BY st;
+
+SELECT est_number,
+company,
+city,
+st,
+zip
+FROM meat_poultry_egg_inspect
+WHERE st IS NULL;
+
+SELECT city, st, zip
+FROM meat_poultry_egg_inspect
+WHERE city = 'Blaine' AND zip = '55449';
+
+SELECT city, st, zip
+FROM meat_poultry_egg_inspect
+WHERE zip = '36671'
+
+SELECT city, st, zip
+FROM meat_poultry_egg_inspect
+WHERE zip = '53538'
+
+SELECT company,
+count(*) AS company_count
+FROM meat_poultry_egg_inspect
+GROUP BY company
+ORDER BY company ASC;
+
+SELECT length(zip),
+count(*) AS length_count
+FROM meat_poultry_egg_inspect
+GROUP BY length(zip)
+ORDER BY length(zip) ASC;
+
+SELECT st,
+count(*) AS st_count
+FROM meat_poultry_egg_inspect
+WHERE length(zip) < 5
+GROUP BY st
+ORDER BY st ASC;
+
+UPDATE meat_poultry_egg_inspect
+SET st = 'MN'
+WHERE est_number = 'V18677A' AND city = 'Blaine';
+
+CREATE TABLE meat_poultry_egg_inspect_backup AS
+SELECT * FROM meat_poultry_egg_inspect;
+
+SELECT
+(SELECT count(*) FROM meat_poultry_egg_inspect) AS original,
+(SELECT count(*) FROM meat_poultry_egg_inspect_backup) AS backup;
+
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN st_copy varchar(2);
+
+UPDATE meat_poultry_egg_inspect
+SET st_copy = st;
+
+SELECT st,
+st_copy
+FROM meat_poultry_egg_inspect
+ORDER BY st;
+
+UPDATE meat_poultry_egg_inspect
+SET st = 'AL'
+WHERE est_number = 'M45319+P45319';
+
+UPDATE meat_poultry_egg_inspect
+SET st = 'WI'
+WHERE est_number = 'M263A+P263A+V263A';
+
+UPDATE meat_poultry_egg_inspect
+SET st = st_copy;
+
+UPDATE meat_poultry_egg_inspect original
+SET st = backup.st
+FROM meat_poultry_egg_inspect_backup backup
+WHERE original.est_number = backup.est_number;
+
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN company_standard varchar(100);
+
+UPDATE meat_poultry_egg_inspect
+SET company_standard = company;
+
+UPDATE meat_poultry_egg_inspect
+SET company_standard = 'Armour-Eckrich Meats'
+WHERE company LIKE 'Armour%';
+
+SELECT company, company_standard
+FROM meat_poultry_egg_inspect
+WHERE company LIKE 'Armour%';
+
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN zip_copy varchar(5);
+
+UPDATE meat_poultry_egg_inspect
+SET zip_copy = zip;
+
+UPDATE meat_poultry_egg_inspect
+SET zip = '00' || zip
+WHERE st IN('PR','VI') AND length(zip) = 3;
+
+UPDATE meat_poultry_egg_inspect
+SET zip = '0' || zip
+WHERE st IN('CT','MA','ME','NH','NJ','RI','VT') AND length(zip) = 4;
+
+CREATE TABLE state_regions (
+st varchar(2) CONSTRAINT st_key PRIMARY KEY,
+region varchar(20) NOT NULL
+);
+
+COPY state_regions
+FROM 'C:\Users\User\Desktop\SQL\practical-sql-master\Chapter_09\state_regions.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN inspection_date date;
+
+UPDATE meat_poultry_egg_inspect inspect
+SET inspection_date = '2019-12-01'
+WHERE EXISTS (SELECT state_regions.region
+FROM state_regions
+WHERE inspect.st = state_regions.st 
+AND state_regions.region = 'New England');
+
+SELECT st, inspection_date
+FROM meat_poultry_egg_inspect
+GROUP BY st, inspection_date
+ORDER BY st;
+
+DELETE FROM meat_poultry_egg_inspect
+WHERE st IN('PR','VI');
+
+ALTER TABLE meat_poultry_egg_inspect DROP COLUMN zip_copy;
+
+DROP TABLE meat_poultry_egg_inspect_backup;
+
+START TRANSACTION;
+
+UPDATE meat_poultry_egg_inspect
+SET company = 'AGRO Merchantss Oakland LLC'
+WHERE company = 'AGRO Merchants Oakland, LLC';
+
+SELECT company
+FROM meat_poultry_egg_inspect
+WHERE company LIKE 'AGRO%'
+ORDER BY company;
+
+ROLLBACK;
--- Classwork/TryItYourselfs.sql
+++ Classwork/TryItYourselfs.sql
@@ -370,3 +370,285 @@ Yes. We would have to know if other companies would like to sell the albums as w
 #3
 To speed up the queries artisits and titles of the album will be good columns candidates for the indexes.
 
+Chap8
+Page 163
+
+CREATE TABLE pls_fy2014_pupld14a (
+    stabr varchar(2) NOT NULL,
+    fscskey varchar(6) CONSTRAINT fscskey2014_key PRIMARY KEY,
+    libid varchar(20) NOT NULL,
+    libname varchar(100) NOT NULL,
+    obereg varchar(2) NOT NULL,
+    rstatus integer NOT NULL,
+    statstru varchar(2) NOT NULL,
+    statname varchar(2) NOT NULL,
+    stataddr varchar(2) NOT NULL,
+    longitud numeric(10,7) NOT NULL,
+    latitude numeric(10,7) NOT NULL,
+    fipsst varchar(2) NOT NULL,
+    fipsco varchar(3) NOT NULL,
+    address varchar(35) NOT NULL,
+    city varchar(20) NOT NULL,
+    zip varchar(5) NOT NULL,
+    zip4 varchar(4) NOT NULL,
+    cnty varchar(20) NOT NULL,
+    phone varchar(10) NOT NULL,
+    c_relatn varchar(2) NOT NULL,
+    c_legbas varchar(2) NOT NULL,
+    c_admin varchar(2) NOT NULL,
+    geocode varchar(3) NOT NULL,
+    lsabound varchar(1) NOT NULL,
+    startdat varchar(10),
+    enddate varchar(10),
+    popu_lsa integer NOT NULL,
+    centlib integer NOT NULL,
+    branlib integer NOT NULL,
+    bkmob integer NOT NULL,
+    master numeric(8,2) NOT NULL,
+    libraria numeric(8,2) NOT NULL,
+    totstaff numeric(8,2) NOT NULL,
+    locgvt integer NOT NULL,
+    stgvt integer NOT NULL,
+    fedgvt integer NOT NULL,
+    totincm integer NOT NULL,
+    salaries integer,
+    benefit integer,
+    staffexp integer,
+    prmatexp integer NOT NULL,
+    elmatexp integer NOT NULL,
+    totexpco integer NOT NULL,
+    totopexp integer NOT NULL,
+    lcap_rev integer NOT NULL,
+    scap_rev integer NOT NULL,
+    fcap_rev integer NOT NULL,
+    cap_rev integer NOT NULL,
+    capital integer NOT NULL,
+    bkvol integer NOT NULL,
+    ebook integer NOT NULL,
+    audio_ph integer NOT NULL,
+    audio_dl float NOT NULL,
+    video_ph integer NOT NULL,
+    video_dl float NOT NULL,
+    databases integer NOT NULL,
+    subscrip integer NOT NULL,
+    hrs_open integer NOT NULL,
+    visits integer NOT NULL,
+    referenc integer NOT NULL,
+    regbor integer NOT NULL,
+    totcir integer NOT NULL,
+    kidcircl integer NOT NULL,
+    elmatcir integer NOT NULL,
+    loanto integer NOT NULL,
+    loanfm integer NOT NULL,
+    totpro integer NOT NULL,
+    totatten integer NOT NULL,
+    gpterms integer NOT NULL,
+    pitusr integer NOT NULL,
+    wifisess integer NOT NULL,
+    yr_sub integer NOT NULL
+);
+
+CREATE INDEX libname2014_idx ON pls_fy2014_pupld14a (libname);
+CREATE INDEX stabr2014_idx ON pls_fy2014_pupld14a (stabr);
+CREATE INDEX city2014_idx ON pls_fy2014_pupld14a (city);
+CREATE INDEX visits2014_idx ON pls_fy2014_pupld14a (visits);
+
+COPY pls_fy2014_pupld14a
+FROM 'C:\Users\User\Desktop\SQL\practical-sql-master\Chapter_08\pls_fy2014_pupld14a.csv'
+WITH (FORMAT CSV, HEADER);
+
+CREATE TABLE pls_fy2009_pupld09a (
+    stabr varchar(2) NOT NULL,
+    fscskey varchar(6) CONSTRAINT fscskey2009_key PRIMARY KEY,
+    libid varchar(20) NOT NULL,
+    libname varchar(100) NOT NULL,
+    address varchar(35) NOT NULL,
+    city varchar(20) NOT NULL,
+    zip varchar(5) NOT NULL,
+    zip4 varchar(4) NOT NULL,
+    cnty varchar(20) NOT NULL,
+    phone varchar(10) NOT NULL,
+    c_relatn varchar(2) NOT NULL,
+    c_legbas varchar(2) NOT NULL,
+    c_admin varchar(2) NOT NULL,
+    geocode varchar(3) NOT NULL,
+    lsabound varchar(1) NOT NULL,
+    startdat varchar(10),
+    enddate varchar(10),
+    popu_lsa integer NOT NULL,
+    centlib integer NOT NULL,
+    branlib integer NOT NULL,
+    bkmob integer NOT NULL,
+    master numeric(8,2) NOT NULL,
+    libraria numeric(8,2) NOT NULL,
+    totstaff numeric(8,2) NOT NULL,
+    locgvt integer NOT NULL,
+    stgvt integer NOT NULL,
+    fedgvt integer NOT NULL,
+    totincm integer NOT NULL,
+    salaries integer,
+    benefit integer,
+    staffexp integer,
+    prmatexp integer NOT NULL,
+    elmatexp integer NOT NULL,
+    totexpco integer NOT NULL,
+    totopexp integer NOT NULL,
+    lcap_rev integer NOT NULL,
+    scap_rev integer NOT NULL,
+    fcap_rev integer NOT NULL,
+    cap_rev integer NOT NULL,
+    capital integer NOT NULL,
+    bkvol integer NOT NULL,
+    ebook integer NOT NULL,
+    audio integer NOT NULL,
+    video integer NOT NULL,
+    databases integer NOT NULL,
+    subscrip integer NOT NULL,
+    hrs_open integer NOT NULL,
+    visits integer NOT NULL,
+    referenc integer NOT NULL,
+    regbor integer NOT NULL,
+    totcir integer NOT NULL,
+    kidcircl integer NOT NULL,
+    loanto integer NOT NULL,
+    loanfm integer NOT NULL,
+    totpro integer NOT NULL,
+    totatten integer NOT NULL,
+    gpterms integer NOT NULL,
+    pitusr integer NOT NULL,
+    yr_sub integer NOT NULL,
+    obereg varchar(2) NOT NULL,
+    rstatus integer NOT NULL,
+    statstru varchar(2) NOT NULL,
+    statname varchar(2) NOT NULL,
+    stataddr varchar(2) NOT NULL,
+    longitud numeric(10,7) NOT NULL,
+    latitude numeric(10,7) NOT NULL,
+    fipsst varchar(2) NOT NULL,
+    fipsco varchar(3) NOT NULL
+);
+
+CREATE INDEX libname2009_idx ON pls_fy2009_pupld09a (libname);
+CREATE INDEX stabr2009_idx ON pls_fy2009_pupld09a (stabr);
+CREATE INDEX city2009_idx ON pls_fy2009_pupld09a (city);
+CREATE INDEX visits2009_idx ON pls_fy2009_pupld09a (visits);
+
+COPY pls_fy2009_pupld09a
+FROM 'C:\Users\User\Desktop\SQL\practical-sql-master\Chapter_08\pls_fy2009_pupld09a.csv'
+WITH (FORMAT CSV, HEADER);
+
+#1
+SELECT pls14.stabr,
+sum(pls14.gpterms) AS connected_pc_2014,
+sum(pls09.gpterms) AS connected_pc_2009,
+round( (CAST(sum(pls14.gpterms) AS decimal(10,1)) - sum(pls09.gpterms)) /
+sum(pls09.gpterms) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.gpterms >= 0 AND pls09.gpterms >= 0
+GROUP BY pls14.stabr
+ORDER BY pct_change DESC;
+
+SELECT pls14.stabr,
+sum(pls14.pitusr) AS connected_pc_2014,
+sum(pls09.pitusr) AS connected_pc_2009,
+round( (CAST(sum(pls14.pitusr) AS decimal(10,1)) - sum(pls09.pitusr)) /
+sum(pls09.pitusr) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.pitusr >= 0 AND pls09.pitusr >= 0
+GROUP BY pls14.stabr
+ORDER BY pct_change DESC;
+
+#2
+SELECT pls14.obereg,
+sum(pls14.visits) AS visits_2014,
+sum(pls09.visits) AS visits_2009,
+round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY pls14.obereg
+ORDER BY pct_change DESC;
+
+CREATE TABLE obereg_codes (
+    obereg varchar(2) CONSTRAINT obereg_key PRIMARY KEY,
+    region varchar(50)
+);
+
+INSERT INTO obereg_codes
+VALUES ('01', 'New England (CT ME MA NH RI VT)'),
+('02', 'Mid East (DE DC MD NJ NY PA)'),
+('03', 'Great Lakes (IL IN MI OH WI)'),
+('04', 'Plains (IA KS MN MO NE ND SD)'),
+('05', 'Southeast (AL AR FL GA KY LA MS NC SC TN VA WV)'),
+('06', 'Soutwest (AZ NM OK TX)'),
+('07', 'Rocky Mountains (CO ID MT UT WY)'),
+('08', 'Far West (AK CA HI NV OR WA)'),
+('09', 'Outlying Areas (AS GU MP PR VI)');
+
+SELECT obereg_codes.region,
+sum(pls14.visits) AS visits_2014,
+sum(pls09.visits) AS visits_2009,
+round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+JOIN obereg_codes
+ON pls14.obereg = obereg_codes.obereg
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY obereg_codes.region
+ORDER BY pct_change DESC;
+
+#3
+SELECT pls14.libname, pls14.city, pls14.stabr, pls14.statstru, pls14.c_admin, pls14.branlib,
+pls09.libname, pls09.city, pls09.stabr, pls09.statstru, pls09.c_admin, pls09.branlib
+FROM pls_fy2014_pupld14a pls14 FULL OUTER JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.fscskey IS NULL OR pls09.fscskey IS NULL;
+
+Chap9
+Page188
+
+CREATE TABLE meat_poultry_egg_inspect (
+est_number varchar(50) CONSTRAINT est_number_key PRIMARY KEY,
+company varchar(100),
+street varchar(100),
+city varchar(30),
+st varchar(2),
+zip varchar(5),
+phone varchar(14),
+grant_date date,
+activities text,
+dbas text
+);
+
+COPY meat_poultry_egg_inspect
+FROM 'C:\Users\User\Desktop\SQL\practical-sql-master\Chapter_09\MPI_Directory_by_Establishment_Name.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+#1
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN meat_processing boolean;
+ALTER TABLE meat_poultry_egg_inspect ADD COLUMN poultry_processing boolean;
+
+SELECT * FROM meat_poultry_egg_inspect;
+
+#2
+UPDATE meat_poultry_egg_inspect
+SET meat_processing = TRUE
+WHERE activities ILIKE '%Meat Processing%';
+
+UPDATE meat_poultry_egg_inspect
+SET poultry_processing = TRUE
+WHERE activities ILIKE '%Poultry Processing%';
+
+SELECT * FROM meat_poultry_egg_inspect;
+
+#3
+SELECT count(meat_processing), count(poultry_processing)
+FROM meat_poultry_egg_inspect;
+
+SELECT count(*)
+FROM meat_poultry_egg_inspect
+WHERE meat_processing = TRUE AND poultry_processing = TRUE;
