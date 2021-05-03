d8cc2f76a474f85fa092bf44a4184737f8c0d5c2
Trystan <trystandames08@gmail.com>
2021-05-03 13:06:39 +0200

    sixteenth commit

--- Projects/Postgre + Python/app.py
+++ Projects/Postgre + Python/app.py
@@ -1,3 +1,5 @@
+from database import add_entry, get_entries
+
 menu = """Please select one of the following optios:
 1) Add new entry for today.
 2) View entries.
@@ -7,12 +9,24 @@ Your Selection: """
 
 welcome = "Welcome to the programming diary!"
 
+def prompt_new_entry():
+	entry_content = input("What have you learned today? ")
+	entry_date = input("Enter the date: ")
+
+	add_entry(entry_content, entry_date)
+
+def view_entries(entries):
+	for entry in entries:
+		print(f"{entry['date']}\n{entry['content']}\n\n")
+
 print(welcome)
 
 while (user_input := input(menu)) != "3":
 	if user_input == "1":
-		print("Adding...")
+		prompt_new_entry()
+
 	elif user_input == "2":
-		print("Viewing...")
+		view_entries(get_entries())
+		
 	else:
 		print("Invalid option, please try again!")
