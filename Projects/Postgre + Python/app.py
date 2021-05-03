fe2ffd36bc30ad0f753e43e4fde139e619ab6607
Trystan <trystandames08@gmail.com>
2021-05-03 11:31:53 +0200

    fifteenth commit

---
+++ Projects/Postgre + Python/app.py
@@ -0,0 +1,18 @@
+menu = """Please select one of the following optios:
+1) Add new entry for today.
+2) View entries.
+3) Exit.
+
+Your Selection: """
+
+welcome = "Welcome to the programming diary!"
+
+print(welcome)
+
+while (user_input := input(menu)) != "3":
+	if user_input == "1":
+		print("Adding...")
+	elif user_input == "2":
+		print("Viewing...")
+	else:
+		print("Invalid option, please try again!")
