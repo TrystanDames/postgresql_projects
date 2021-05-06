e641a05d691b1834d6c69ebaecf74ed7a34c18cc
Trystan <trystandames08@gmail.com>
2021-05-06 11:29:44 +0200

    27th commit

--- Projects/Postgre + Python/Movie/database.py
+++ Projects/Postgre + Python/Movie/database.py
@@ -11,6 +11,8 @@ INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp, watched) VALUES (
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
 SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
+SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?"
+DELETE_MOVIE = "DELETE FROM movies WHERE title = ?"
 
 connection = sqlite3.connect("data.db")
 
@@ -26,14 +28,15 @@ def get_movies(upcoming = False):
 	with connection:
 		cursor = connection.cursor()
 		if upcoming:
-			today_timestamp = datetime.datetime.todaye().timestamp()
+			today_timestamp = datetime.datetime.today().timestamp()
 			cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
 		else:
 			cursor.execute(SELECT_ALL_MOVIES)
 		return cursor.fetchall()
 
 def watch_movie(title):
-	
+	with connection:
+		connection.execute(SET_MOVIE_WATCHED, (title,))
 
 def get_watched_movies():
 	with connection:
